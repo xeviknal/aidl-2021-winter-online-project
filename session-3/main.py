5b2da9ba112865c30b9dcbbb3da2297f61cecaf4
Xavier Canal i Masjuan <xeviknal@gmail.com>
2021-01-27 20:07:27 +0100

    Adding basic flow

--- session-3/main.py
+++ session-3/main.py
@@ -6,57 +6,74 @@ from utils import binary_accuracy
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 import torch.optim as optim
-import torch.nn.functional as F
+import torch.nn as nn
 import numpy as np
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
 
-def train_single_epoch(model, train_loader, optimizer):
+def train_single_epoch(model, train_loader, optimizer, criterion):
     model.train()
     accs, losses = [], []
     for x, y in train_loader:
-        # You will need to do y = y.unsqueeze(1).float() to add an output dimension to the labels and cast to the correct type
-        ...
-        losses.append(loss.item())
+        x, y = x.to(device), y.to(device)
+        y = y.unsqueeze(1).float()
+        optimizer.zero_grad()
+        output = model(x)
+        loss = criterion(output, y)
+        loss.backward()
+        optimizer.step()
+
+        acc = binary_accuracy(y, output)
         accs.append(acc.item())
     return np.mean(losses), np.mean(accs)
 
 
-def eval_single_epoch(model, val_loader):
+def eval_single_epoch(model, val_loader, criterion):
     accs, losses = [], []
     with torch.no_grad():
         model.eval()
         for x, y in val_loader:
-            ...
+            x, y = x.to(device), y.to(device)
+            y = y.unsqueeze(1).float()
+            output = model(x)
+            loss = criterion(output, y)
             losses.append(loss.item())
+            acc = binary_accuracy(y, output)
             accs.append(acc.item())
+
     return np.mean(losses), np.mean(accs)
 
 
 def train_model(config):
 
-    data_transforms = transforms.Compose([...])
-    train_dataset = ImageFolder...
+    data_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.CenterCrop(110),
+        transforms.ToTensor(),
+    ])
+    train_dataset = ImageFolder(train_dir, transform=data_transforms)
     train_loader = DataLoader(train_dataset, batch_size=config["batch_size"], shuffle=True)
-    test_dataset = ImageFolder...
+    test_dataset = ImageFolder(test_dir, transform=data_transforms)
     test_loader = DataLoader(test_dataset, batch_size=config["batch_size"])
 
     my_model = MyModel().to(device)
 
+    criterion = nn.BCELoss()
     optimizer = optim.Adam(my_model.parameters(), config["lr"])
     for epoch in range(config["epochs"]):
-        loss, acc = train_single_epoch(my_model, train_loader, optimizer)
+        loss, acc = train_single_epoch(my_model, train_loader, optimizer, criterion)
         print(f"Train Epoch {epoch} loss={loss:.2f} acc={acc:.2f}")
-        loss, acc = eval_single_epoch(my_model, test_loader)
+        loss, acc = eval_single_epoch(my_model, test_loader, criterion)
         print(f"Eval Epoch {epoch} loss={loss:.2f} acc={acc:.2f}")
     
     return my_model
 
 
 if __name__ == "__main__":
-
+    train_dir = './dataset/cars_vs_flowers/training_set/'
+    test_dir = './dataset/cars_vs_flowers/test_set/'
     config = {
         "lr": 1e-3,
         "batch_size": 64,
--- session-3/model.py
+++ session-3/model.py
@@ -1,12 +1,29 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
+
 
 class MyModel(nn.Module):
 
     def __init__(self):
         super().__init__()
-        ...
+        self.pipeline = nn.Sequential( # [128, 128]
+            nn.Conv2d(3, 32, 3), # [32, 126, 126]
+            nn.ReLU(),
+            nn.MaxPool2d(2), # [32, 63, 63]
+            nn.Conv2d(32, 64, 3), # [64, 61, 61]
+            nn.ReLU(),
+            nn.MaxPool2d(2), # [64, 30, 30]
+            nn.Conv2d(64, 128, 3), # [128, 27, 27]
+            nn.ReLU(),
+            nn.MaxPool2d(2), # [128, 13, 13]
+            nn.Conv2d(128, 256, 3), # [256, 11, 11]
+            nn.ReLU(),
+            nn.MaxPool2d(2), # [256, 5, 5]
+            nn.Flatten(),
+            nn.Linear(256*5*5, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1),
+            nn.Sigmoid()
+        )
 
     def forward(self, x):
-        ...
+        return self.pipeline(x)
